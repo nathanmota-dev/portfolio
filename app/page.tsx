e3bdf4f6aeb08f5c8c11fd910fae27043b3a80c9
Nathan <nathansmota@gmail.com>
2024-05-28 20:09:07 -0300

    :tada: initial commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-6">
+      <h1 className="text-4xl font-bold">Teste</h1>
+    </main>
+  );
+}
