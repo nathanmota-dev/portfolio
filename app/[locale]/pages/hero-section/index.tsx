d6b6e87ea75c9117b94d7ca9f3eda4a405b752c4
Nathan <nathansmota@gmail.com>
2024-06-01 03:43:29 -0300

    :lipstick: feat: update translate and responsive

--- app/[locale]/pages/hero-section/index.tsx
+++ app/[locale]/pages/hero-section/index.tsx
@@ -3,6 +3,9 @@ import { TechBadge } from "../../components/tech-badge";
 import { Button } from "../../components/button";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import { TbBrandGithub, TbBrandGmail, TbBrandLinkedin, TbBrandWhatsapp } from "react-icons/tb";
+import { useTranslations } from 'next-intl'
+import { FaRegArrowAltCircleDown } from "react-icons/fa";
+import { BiMouse } from "react-icons/bi";
 
 const contacts = [
     {
@@ -24,13 +27,18 @@ const contacts = [
 ]
 
 export default function HeroSection() {
+
+    const t = useTranslations("Hero");
+
     return (
-        <section className="bg-white dark:bg-black w-full h-[91vh] flex flex-col justify-center items-center">
+        <section className="bg-white dark:bg-black w-full h-[91.5vh] flex flex-col justify-center items-center">
             <div className="container flex items-center justify-between flex-col-reverse lg:flex-row mx-auto px-4">
                 <div className="w-full lg:max-w-[530px] text-center lg:text-left">
-                    <p className="font-mono text-emerald-600 dark:text-emerald-400">Olá, meu nome é</p>
-                    <h2 className="text-4xl font-medium mt-2">Nathan Mota</h2>
-                    <p className="text-gray-800 dark:text-gray-400 my-6 text-sm sm:text-base">Olá, meu nome é Nathan Mota e sou desenvolvedor Full-stack com foco em Front-end.</p>
+                    <p className="font-mono text-emerald-600 dark:text-emerald-400">{t("p")}</p>
+                    <h2 className="text-4xl font-medium mt-2">{t("h2")}</h2>
+                    <p className="text-gray-800 dark:text-gray-400 my-6 text-sm sm:text-base">
+                        {t("text")}
+                    </p>
 
                     <div className="flex flex-wrap gap-x-2 gap-y-3 lg:max-w-[340px] justify-center lg:justify-start">
                         <TechBadge name="JavaScript" />
@@ -41,13 +49,13 @@ export default function HeroSection() {
                         <TechBadge name="Next.js" />
                     </div>
 
-                    <div className="mt-6 lg:mt-10 flex sm:items-center sm:gap-5 flex-col sm:flex-row justify-center lg:justify-start">
+                    <div className="mt-6 lg:mt-10 flex items-center sm:gap-5 flex-col sm:flex-row justify-center lg:justify-start">
                         <Button className="w-max shadow-button flex items-center gap-2">
-                            Entre em contato
+                            {t("button")}
                             <HiArrowNarrowRight size={18} />
                         </Button>
 
-                        <div className="text-2xl flex items-center gap-3">
+                        <div className="text-2xl flex items-center gap-3 mt-4 sm:mt-0">
                             {contacts.map((contact, index) => (
                                 <a
                                     href={contact.url}
@@ -68,6 +76,12 @@ export default function HeroSection() {
                     alt="Foto de perfil de Nathan Mota"
                     className="w-[300px] h-[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 rounded-lg shadow-2xl object-cover"
                 />
+            </div>
+
+            <div className="sm:text-2xl flex items-center gap-3 mt-6 sm:mt-48 font-bold text-gray-800 dark:text-gray-400 hover:text-emerald-600 dark:hover:text-emerald-400 transition-colors">
+                <BiMouse />
+                <h2 className="sm:text-lg">{t("rolar")}</h2>
+                <FaRegArrowAltCircleDown />
             </div>
         </section>
     )
