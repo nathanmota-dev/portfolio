cd085380cbf5e470f004a911360847276b967e74
Nathan <nathansmota@gmail.com>
2024-06-04 16:14:07 -0300

    :recycle: refactor: refactored component to accept icon

--- app/[locale]/components/tech-badge/index.tsx
+++ app/[locale]/components/tech-badge/index.tsx
@@ -1,10 +1,14 @@
+import { IconType } from 'react-icons';
+
 type TechBadgeProps = {
     name: string;
+    Icon?: IconType;
 }
 
-export const TechBadge = ({ name }: TechBadgeProps) => {
+export const TechBadge = ({ name, Icon }: TechBadgeProps) => {
     return (
-        <span className="text-emerald-200 dark:text-emerald-400 bg-emerald-700/80 dark:bg-emerald-900/80 text-sm py-1 px-3 rounded-lg">
+        <span className="flex items-center text-emerald-200 dark:text-emerald-400 bg-emerald-700/80 dark:bg-emerald-900/80 text-sm py-1 px-3 rounded-lg">
+            {Icon && <Icon className="mr-2" />}
             {name}
         </span>
     )
