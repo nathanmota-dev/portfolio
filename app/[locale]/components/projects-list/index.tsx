3758a3681d019c28ee6c511c6182dc9522d90493
Nathan <nathansmota@gmail.com>
2024-06-20 23:59:24 -0300

    :sparkles: feat: update projects

--- app/[locale]/components/projects-list/index.tsx
+++ app/[locale]/components/projects-list/index.tsx
@@ -1,16 +1,88 @@
 import { ProjectCard } from "./project-card";
+import { FaCss3Alt, FaHtml5, FaNodeJs, FaReact, } from "react-icons/fa";
+import { RiJavascriptFill } from "react-icons/ri";
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiExpress } from "react-icons/si";
+import { BiLogoTypescript } from "react-icons/bi";
+import CoffeeLP from "../../../../public/projects/Coffee-LP.jpg"
+import InsoveLP from "../../../../public/projects/Insove-LP.jpg"
+import Buscador from "../../../../public/projects/Buscador.jpg"
+import Dash from "../../../../public/projects/Dash.jpg"
+import ListaDePaises from "../../../../public/projects/ListaDePaises.jpg"
+import GoogleKeep from "../../../../public/projects/GoogleKeep.jpg"
+import NodeFastify from "../../../../public/projects/NodeFastify.jpg"
+
+const techIcons = {
+    "React": FaReact,
+    "Next": TbBrandNextjs,
+    "Node": FaNodeJs,
+    "JavaScript": RiJavascriptFill,
+    "TypeScript": BiLogoTypescript,
+    "CSS": FaCss3Alt,
+    "HTML": FaHtml5,
+    "Express": SiExpress,
+};
+
+const projectsList = [    
+    {
+        image: CoffeeLP,
+        title: "Landing Page Meow Café",
+        description: "Projeto proposto pela Codante onde o objetivo foi dar a vida ao design do Meow Café transformando-o em uma página funcional e responsiva sem o uso de frameworks.",
+        technologies: ["HTML", "CSS", "JavaScript"]
+    },
+    {
+        image: InsoveLP,
+        title: "Landing Page Insove Medical Healthcare",
+        description: "Landing Page feita utilizando TailwindCSS projetada para consultórios médicos onde o objetivo foi proporcionar uma boa experiência para médicos e pacientes.",
+        technologies: ["HTML", "TailwindCSS", "Remix Icons", "SwiperJS", "Scroll Reveal"]
+    },
+    {
+        image: ListaDePaises,
+        title: "Lista de Países",
+        description: "Projeto proposto pela Codante onde o objetivo foi utilizar uma API para objter os dados fazendo uma aplicação reponsiva que use Server Components, Data fetching, Nested layouts, Routes e Loading",
+        technologies: ["Next", "TypeScript", "TailwindCSS"]
+    },
+    {
+        image: Buscador,
+        title: "Buscador de Nomes",
+        description: "Projeto Final de Programação para Internet onde o objetivo foi criar um buscador de nomes consumindo uma API no backend, tratando os dados e exibindo no frontend com um histórico de consultas.",
+        technologies: ["React", "Node", "Express", "MySQL"]
+    },
+    {
+        image: Dash,
+        title: "Dashboard",
+        description: "Dashboard com página de Login, Cadastro, autenticação, criptografia e um DashBoard",
+        technologies: ["React", "Vite", "Node", "Express", "MySQL", "SCSS", "ReactHookForm", "React Icons", "Yup.js"]
+    },
+    {
+        image: GoogleKeep,
+        title: "Google Keep Clone",
+        description: "Clone do Google Keep proposto a ser realizado sem frameworks onde permite que você faça anotações, fixe notas, duplique notas e exporte as notas para um arquivo CSV.",
+        technologies: ["HTML", "CSS", "JavaScript"]
+    },
+    {
+        image: NodeFastify,
+        title: "Node Fastify",
+        description: "API feita com o objetivo de realizar um CRUD utilizando um banco de dados relacional",
+        technologies: ["Node", "Fastify", "PostgreSQL"]
+    }
+]
 
 
 export const ProjectList = () => {
 
     return (
         <section className="container py-20 grid grid-cols-1 sm:grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-x-6 gap-y-6">
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
+            {projectsList.map((project, index) => (
+                <ProjectCard
+                    key={index}
+                    image={project.image}
+                    title={project.title}
+                    description={project.description}
+                    technologies={project.technologies}
+                    techIcons={techIcons}
+                />
+            ))}
         </section>
     );
 }
--- app/[locale]/components/projects-list/project-card/index.tsx
+++ app/[locale]/components/projects-list/project-card/index.tsx
@@ -1,15 +1,16 @@
-import Image from "next/image";
-import { FaCss3Alt, FaHtml5, FaNodeJs, FaReact, } from "react-icons/fa";
+import Image, { StaticImageData } from "next/image";
 import { TechBadge } from "../../tech-badge";
-import { RiJavascriptFill } from "react-icons/ri";
-import { TbBrandNextjs } from "react-icons/tb";
-import { SiExpress } from "react-icons/si";
-import { BiLogoTypescript } from "react-icons/bi";
-import { useTranslations } from "next-intl";
+import { IconType } from "react-icons";
 
-export const ProjectCard = () => {
+type ProjectCardProps = {
+    image: StaticImageData;
+    title: string;
+    description: string;
+    technologies: string[];
+    techIcons: { [key: string]: IconType };
+}
 
-    const t = useTranslations("Card");
+export const ProjectCard = ({ image, title, description, technologies, techIcons }: ProjectCardProps) => {
 
     return (
         <div className="rounded-lg h-[550px] flex flex-col bg-white dark:bg-zinc-800  overflow-hidden border-2 border-gray-200 dark:border-gray-800 hover:border-emerald-500 opacity-70 hover:opacity-100 transition-all group">
@@ -17,8 +18,8 @@ export const ProjectCard = () => {
                 <Image
                     width={380}
                     height={200}
-                    src="https://hermes.dio.me/articles/cover/7c856878-99cc-488b-9409-0cbc92cf7b66.png"
-                    alt="Projeto 1"
+                    src={image}
+                    alt={title}
                     unoptimized
                     className="w-full h-full object-cover group-hover:scale-105 transition-all duration-500 ease-in-out"
                 />
@@ -26,20 +27,18 @@ export const ProjectCard = () => {
 
             <div className="flex flex-1 flex-col p-8">
                 <strong className="font-medim text-gray-600 text-gray-400/80 dark:text-gray-400 dark:text-gray-50/90 group-hover:text-emerald-500 transition-all">
-                    {t("name")}
+                    {title}
                 </strong>
-                <p className="mt-2 text-gray-500 dark:text-gray-400 line-clamp-4">
-                    {t("description")}
+                <p className="mt-2 text-gray-500 dark:text-gray-400 line-clamp-5">
+                    {description}
                 </p>
                 <span className="mx-auto py-3 gap-x-2 gap-y-2 flex flex-wrap justify-center max-w-max">
-                    <TechBadge name="JavaScript" Icon={RiJavascriptFill} />
-                    <TechBadge name="TypeScript" Icon={BiLogoTypescript} />
-                    <TechBadge name="ReactJs" Icon={FaReact} />
-                    <TechBadge name="NodeJs" Icon={FaNodeJs} />
-                    <TechBadge name="Express" Icon={SiExpress} />
-                    <TechBadge name="Next.js" Icon={TbBrandNextjs} />
-                    <TechBadge name="HTML" Icon={FaHtml5} />
-                    <TechBadge name="CSS" Icon={FaCss3Alt} />
+                    {technologies.map((tech, index) => {
+                        const IconComponent = techIcons[tech];
+                        return (
+                            <TechBadge key={index} name={tech} Icon={IconComponent} />
+                        );
+                    })}
                 </span>
             </div>
         </div>
